a0016b5acc5a6e31a739389337227699751a19bd
LIN Zeyin <75190045+fjljlzy@users.noreply.github.com>
2021-09-01 14:47:13 +0800

    Update solution.py

--- Array/Two Sum/solution.py
+++ Array/Two Sum/solution.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for index, i in enumerate(nums):
+            diff = target - i
+            if diff in d:
+                return [index, d[diff]]
+            else:
+                d[i] = index
