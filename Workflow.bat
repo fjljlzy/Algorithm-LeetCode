5e02e50c68991b918f78fc83772d9226e427927c
LIN Zeyin <75190045+fjljlzy@users.noreply.github.com>
2021-09-06 17:41:10 +0800

    Add files via upload
    Workflow can make me study in the library easier and more focus.

---
+++ Workflow.bat
@@ -0,0 +1,3 @@
+@ehco 
+start chrome https://www.notion.so/Hello-Zy-63cb60ae47d14bdd8f6b6eb1bfe68865 https://canvas.ust.hk/courses https://leetcode.com/problemset/all/ https://leetcode-cn.com/problemset/all/ https://translate.google.com/ https://github.com/NLP-LOVE/ML-NLP https://www.youtube.com/watch?v=4mxx5zK63KE&t=7447s
+pause
