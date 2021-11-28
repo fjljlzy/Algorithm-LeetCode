b0629e161a0206fcccbfe9f5a753d613aad1859c
LIN Zeyin <75190045+fjljlzy@users.noreply.github.com>
2021-11-29 03:04:47 +0800

    Create 0104_二叉树的最大深度.py

---
+++ Tree/0104_二叉树的最大深度.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        # DFS(post-order)
+        # if not root:
+        #     return 0
+        # d1 = self.maxDepth(root.left)
+        # d2 = self.maxDepth(root.right)
+        # d = max(d1, d2) + 1
+        # return d
+
+        # DFS(post-order) 2
+        # if not root:
+        #     return 0
+        # return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+        # BFS (only stack)
+        # if not root: return 0
+        # q = [root]
+        # depth = 0
+        # while q:
+        #     depth += 1
+        #     nxt = []
+        #     for node in q:
+        #         if node.left:
+        #             nxt.append(node.left)
+        #         if node.right:
+        #             nxt.append(node.right)
+        #     q = nxt
+        # return depth
+        
